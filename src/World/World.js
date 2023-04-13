23f6ebd9f7e56426552a002f40c0240a8b8c73c2
wpendl99 <57572263+wpendl99@users.noreply.github.com>
2023-04-12 18:21:17 -0600

    Adding Buttons for Non-VR

--- src/World/World.js
+++ src/World/World.js
@@ -10,12 +10,12 @@ import { Loop } from "./systems/Loop.js";
 import { VRControl } from "./systems/VRControl.js";
 import { Vector3 } from "three";
 import {
-  controller1,
-  controller2,
-  controllerGrip1,
-  controllerGrip2,
-  hand1,
-  hand2,
+	controller1,
+	controller2,
+	controllerGrip1,
+	controllerGrip2,
+	hand1,
+	hand2,
 } from "./components/controllers.js";
 import { createControllerPanel } from "./components/controllerPanel.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
@@ -36,186 +36,188 @@ let panorama;
 let panel;
 
 class World {
-  constructor(container, xrEnabledStops) {
-    const _xrEnabledStops = xrEnabledStops;
-    camera = createCamera();
-    scene = createScene();
-    renderer = Renderer.getRenderer();
-    loop = new Loop(camera, scene, renderer);
-    container.append(renderer.domElement);
+	constructor(container, xrEnabledStops) {
+		const _xrEnabledStops = xrEnabledStops;
+		camera = createCamera();
+		scene = createScene();
+		renderer = Renderer.getRenderer();
+		loop = new Loop(camera, scene, renderer);
+		container.append(renderer.domElement);
 
-    const light = createLights();
+		const light = createLights();
 
-    const coordinateList = xrEnabledStops.map((stop) => ({
-      lat: stop.lat,
-      lng: stop.lng,
-    }));
-    const locationList = xrEnabledStops.map((stop) => stop.name);
-    const desc_list = xrEnabledStops.map((stop) => stop.description);
+		const coordinateList = xrEnabledStops.map((stop) => ({
+			lat: stop.lat,
+			lng: stop.lng,
+		}));
+		const locationList = xrEnabledStops.map((stop) => stop.name);
+		const desc_list = xrEnabledStops.map((stop) => stop.description);
 
-    pl_instance = new PanoramaLoader(coordinateList, locationList, desc_list);
-    scene.add(camera, light);
-    let promise = pl_instance.loadPanorama().then(
-      (resolve) => {
-        panorama = resolve.panorama_new;
-        scene.add(panorama);
-        let description = "\n" + resolve.description;
-        panel = createPanel(resolve.locationName, description);
-        scene.add(panel);
-      },
-      (reject) => {
-        console.log(reject);
-        if (reject == "outofbounds") {
-          scene.remove(panel);
-          panel = createPanel("Server error, please refresh the page.");
-          scene.add(panel);
-        } else {
-          scene.remove(panel);
-          panel = createPanel("Server error, please refresh the page.");
-          scene.add(panel);
-        }
-      }
-    );
+		pl_instance = new PanoramaLoader(coordinateList, locationList, desc_list);
+		scene.add(camera, light);
+		let promise = pl_instance.loadPanorama().then(
+			(resolve) => {
+				panorama = resolve.panorama_new;
+				scene.add(panorama);
+				let description = "\n" + resolve.description;
+				panel = createPanel(resolve.locationName, description);
+				scene.add(panel);
+			},
+			(reject) => {
+				console.log(reject);
+				if (reject == "outofbounds") {
+					scene.remove(panel);
+					panel = createPanel("Server error, please refresh the page.");
+					scene.add(panel);
+				} else {
+					scene.remove(panel);
+					panel = createPanel("Server error, please refresh the page.");
+					scene.add(panel);
+				}
+			}
+		);
 
-    // const controls = new OrbitControls(camera, renderer.domElement);
-    // controls.maxPolarAngle = Math.PI * 1;
-    // controls.target = new Vector3(0, 1, -5);
-    // controls.target = new Vector3(0, 0, -0.01);
-    // controls.minDistance = 450;
-    // controls.maxDistance = 450;
+		// const controls = new OrbitControls(camera, renderer.domElement);
+		// controls.maxPolarAngle = Math.PI * 1;
+		// controls.target = new Vector3(0, 1, -5);
+		// controls.target = new Vector3(0, 0, -0.01);
+		// controls.minDistance = 450;
+		// controls.maxDistance = 450;
 
-    // controls.update();
+		// controls.update();
 
-    controls = new OrbitControls(camera, renderer.domElement);
-    controls.target.set(0, 0.5, 0);
-    camera.position.set(0, 0.5, 1);
-    controls.update();
+		controls = new OrbitControls(camera, renderer.domElement);
+		controls.target.set(0, 0.5, 0);
+		camera.position.set(0, 0.5, 1);
+		controls.update();
 
-    const resizer = new Resizer(container, camera, renderer);
+		const resizer = new Resizer(container, camera, renderer);
 
-    //TODO: The below two lines are for local preview vs. production
-    //document.body.appendChild(ARButton.createButton(renderer));
-    document.body.appendChild(VRButton.createButton(renderer));
+		//TODO: The below two lines are for local preview vs. production
+		//document.body.appendChild(ARButton.createButton(renderer));
+		document.body.appendChild(VRButton.createButton(renderer));
 
-    //controllers
-    const _controller1 = controller1;
-    const _controllerGrip1 = controllerGrip1;
-    const _controller2 = controller2;
-    const _controllerGrip2 = controllerGrip2;
+		//controllers
+		const _controller1 = controller1;
+		const _controllerGrip1 = controllerGrip1;
+		const _controller2 = controller2;
+		const _controllerGrip2 = controllerGrip2;
 
-    _controller1.addEventListener("selectstart", onSelectStart1);
-    _controller1.add(createControllerPanel("Next Location"));
-    _controller2.addEventListener("selectstart", onSelectStart2);
-    _controller2.add(createControllerPanel("Previous Location"));
+		document.getElementById("next-button").onclick = onSelectStart1;
+		document.getElementById("prev-button").onclick = onSelectStart2;
+		_controller1.addEventListener("selectstart", onSelectStart1);
+		_controller1.add(createControllerPanel("Next Location"));
+		_controller2.addEventListener("selectstart", onSelectStart2);
+		_controller2.add(createControllerPanel("Previous Location"));
 
-    const button = new ThreeMeshUI.Text({ textContent: "click me" });
-    scene.add(button);
+		const button = new ThreeMeshUI.Text({ textContent: "click me" });
+		scene.add(button);
 
-    scene.add(_controller1, _controllerGrip1, _controller2, _controllerGrip2);
-  }
+		scene.add(_controller1, _controllerGrip1, _controller2, _controllerGrip2);
+	}
 
-  render() {
-    // draw a single frame
-    renderer.render(scene, camera);
-  }
+	render() {
+		// draw a single frame
+		renderer.render(scene, camera);
+	}
 
-  start() {
-    loop.start();
-  }
+	start() {
+		loop.start();
+	}
 
-  stop() {
-    loop.stop();
-  }
+	stop() {
+		loop.stop();
+	}
 
-  getPanorama() {
-    return panorama;
-  }
+	getPanorama() {
+		return panorama;
+	}
 
-  setPanorama(new_panorama) {
-    panorama = new_panorama;
-  }
+	setPanorama(new_panorama) {
+		panorama = new_panorama;
+	}
 
-  getPanel() {
-    return panel;
-  }
+	getPanel() {
+		return panel;
+	}
 
-  setPanel(new_panel) {
-    panel = new_panel;
-  }
+	setPanel(new_panel) {
+		panel = new_panel;
+	}
 
-  getPanoramaLoader() {
-    return pl_instance;
-  }
+	getPanoramaLoader() {
+		return pl_instance;
+	}
 }
 
 function onSelectStart2(e) {
-  scene.remove(panel);
-  let oldPanel = panel;
-  panel = createPanel("Loading previous destination...");
-  scene.add(panel);
+	scene.remove(panel);
+	let oldPanel = panel;
+	panel = createPanel("Loading previous destination...");
+	scene.add(panel);
 
-  let promise = pl_instance.movePanorama("prev").then(
-    (resolve) => {
-      console.log("prev");
+	let promise = pl_instance.movePanorama("prev").then(
+		(resolve) => {
+			console.log("prev");
 
-      scene.remove(panorama);
-      panorama = resolve.panorama_new;
-      scene.add(panorama);
+			scene.remove(panorama);
+			panorama = resolve.panorama_new;
+			scene.add(panorama);
 
-      scene.remove(panel);
-      let description = "\n" + resolve.description;
-      panel = createPanel(resolve.locationName, description);
-      scene.add(panel);
-    },
-    (reject) => {
-      console.log(reject);
-      if (reject == "outofbounds") {
-        scene.remove(panel);
-        panel = oldPanel;
-        scene.add(panel);
-      } else {
-        scene.remove(panel);
-        panel = createPanel("Server error, please refresh the page.", "");
-        scene.add(panel);
-      }
-    }
-  );
-  console.log(promise);
+			scene.remove(panel);
+			let description = "\n" + resolve.description;
+			panel = createPanel(resolve.locationName, description);
+			scene.add(panel);
+		},
+		(reject) => {
+			console.log(reject);
+			if (reject == "outofbounds") {
+				scene.remove(panel);
+				panel = oldPanel;
+				scene.add(panel);
+			} else {
+				scene.remove(panel);
+				panel = createPanel("Server error, please refresh the page.", "");
+				scene.add(panel);
+			}
+		}
+	);
+	console.log(promise);
 }
 
 function onSelectStart1(e) {
-  scene.remove(panel);
-  let oldPanel = panel;
-  panel = createPanel("Loading next destination...");
-  scene.add(panel);
+	scene.remove(panel);
+	let oldPanel = panel;
+	panel = createPanel("Loading next destination...");
+	scene.add(panel);
 
-  let promise = pl_instance.movePanorama("next").then(
-    (resolve) => {
-      console.log("next");
+	let promise = pl_instance.movePanorama("next").then(
+		(resolve) => {
+			console.log("next");
 
-      scene.remove(panorama);
-      panorama = resolve.panorama_new;
-      scene.add(panorama);
+			scene.remove(panorama);
+			panorama = resolve.panorama_new;
+			scene.add(panorama);
 
-      scene.remove(panel);
-      let description = "\n" + resolve.description;
-      panel = createPanel(resolve.locationName, description);
-      scene.add(panel);
-    },
-    (reject) => {
-      console.log(reject);
-      if (reject == "outofbounds") {
-        scene.remove(panel);
-        panel = oldPanel;
-        scene.add(panel);
-      } else {
-        scene.remove(panel);
-        panel = createPanel("Server error, please refresh the page.", "");
-        scene.add(panel);
-      }
-    }
-  );
-  console.log(promise);
+			scene.remove(panel);
+			let description = "\n" + resolve.description;
+			panel = createPanel(resolve.locationName, description);
+			scene.add(panel);
+		},
+		(reject) => {
+			console.log(reject);
+			if (reject == "outofbounds") {
+				scene.remove(panel);
+				panel = oldPanel;
+				scene.add(panel);
+			} else {
+				scene.remove(panel);
+				panel = createPanel("Server error, please refresh the page.", "");
+				scene.add(panel);
+			}
+		}
+	);
+	console.log(promise);
 }
 
 export { World, renderer, scene };
