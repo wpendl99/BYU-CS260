4817bf1d45289cb621caf35349746f52e8c5136b
dan-dean <78292223+dan-dean@users.noreply.github.com>
2023-04-14 18:09:28 -0600

    panoloader cdn

--- public/src/World/components/PanoramaLoader.js
+++ public/src/World/components/PanoramaLoader.js
@@ -1,11 +1,11 @@
 import {
-  SphereGeometry,
-  Mesh,
-  MeshBasicMaterial,
-  TextureLoader,
-  RepeatWrapping,
-  DoubleSide,
-  Texture,
+	SphereGeometry,
+	Mesh,
+	MeshBasicMaterial,
+	TextureLoader,
+	RepeatWrapping,
+	DoubleSide,
+	Texture,
 } from "three";
 
 let texture;
@@ -15,96 +15,99 @@ let _desc_list;
 let _pos;
 
 class PanoramaLoader {
-  constructor(coordinate_list, location_list, desc_list) {
-    _location_list = location_list;
-    _pos = 0;
-    _coordinate_list = coordinate_list;
-    _desc_list = desc_list;
-    console.log(_pos);
-  }
+	constructor(coordinate_list, location_list, desc_list) {
+		_location_list = location_list;
+		_pos = 0;
+		_coordinate_list = coordinate_list;
+		_desc_list = desc_list;
+		console.log(_pos);
+	}
 
-  setPanorama() {
-    texture.wrapS = RepeatWrapping;
-    texture.wrapT = RepeatWrapping;
+	setPanorama() {
+		texture.wrapS = RepeatWrapping;
+		texture.wrapT = RepeatWrapping;
 
-    let panorama = new Mesh(
-      new SphereGeometry(500, 60, 40),
-      new MeshBasicMaterial({ map: texture, side: DoubleSide })
-    );
+		let panorama = new Mesh(
+			new SphereGeometry(500, 60, 40),
+			new MeshBasicMaterial({ map: texture, side: DoubleSide })
+		);
 
-    // panorama.material.side(DoubleSide)
-    // panorama.scale.x = -1;
-    // panorama.scale.y = -1;
-    // panorama.scale.z = -1
-    panorama.tick = (delta) => {};
-    return panorama;
-  }
+		// panorama.material.side(DoubleSide)
+		// panorama.scale.x = -1;
+		// panorama.scale.y = -1;
+		// panorama.scale.z = -1
+		panorama.tick = (delta) => {};
+		return panorama;
+	}
 
-  async movePanorama(move) {
-    return new Promise((resolve, reject) => {
-      if (move == "prev" && _pos > 0) {
-        _pos--;
-      } else if (move == "next" && _pos < _location_list.length - 1) {
-        _pos++;
-      } else {
-        console.log(_pos);
-        reject("outofbounds");
-      }
-      console.log(_pos);
+	async movePanorama(move) {
+		return new Promise((resolve, reject) => {
+			if (move == "prev" && _pos > 0) {
+				_pos--;
+			} else if (move == "next" && _pos < _location_list.length - 1) {
+				_pos++;
+			} else {
+				console.log(_pos);
+				reject("outofbounds");
+			}
+			console.log(_pos);
 
-      this.loadPanorama().then(
-        ({ panorama_new, locationName, description }) => {
-          resolve({ panorama_new, locationName, description });
-        },
-        (error) => {
-          reject(error);
-        }
-      );
-    });
-  }
+			this.loadPanorama().then(
+				({ panorama_new, locationName, description }) => {
+					resolve({ panorama_new, locationName, description });
+				},
+				(error) => {
+					reject(error);
+				}
+			);
+		});
+	}
 
-  async loadPanorama() {
-    return new Promise(async (resolve, reject) => {
-      texture = new Texture();
+	async loadPanorama() {
+		return new Promise(async (resolve, reject) => {
+			// texture = new Texture();
 
-      let image = new Image();
-      image.onload = function () {
-        texture.image = image;
-        texture.needsUpdate = true;
-      };
+			// let image = new Image();
+			// image.onload = function () {
+			//   texture.image = image;
+			//   texture.needsUpdate = true;
+			// };
 
-      let lat = _coordinate_list[_pos].lat;
-      let lon = _coordinate_list[_pos].lng;
-      let zoom = 3;
+			texture = new TextureLoader();
 
-      const response = await fetch(
-        //`http://10.37.250.26:5000/panorama?lat=${lat}&lon=${lon}&zoom=${zoom}`
-        `https://pano.worldexcursions.click/panorama?lat=${lat}&lon=${lon}&zoom=${zoom}`
-      );
-      if (response.ok) {
-        const data = await response.json();
-        const base64Image = data.base64Image;
-        image.src = base64Image;
-        // Do something with the base64Image, e.g., display it on the page
-        // console.log(base64Image);
+			let lat = _coordinate_list[_pos].lat;
+			let lon = _coordinate_list[_pos].lng;
+			let zoom = 3;
 
-        console.log(_location_list[_pos]);
-        resolve({
-          panorama_new: this.setPanorama(),
-          locationName: _location_list[_pos],
-          description: _desc_list[_pos],
-        });
-      } else {
-        // Handle HTTP errors
-        console.error(
-          "Error fetching panorama image:",
-          response.status,
-          response.statusText
-        );
-        reject(response);
-      }
-    });
-  }
+			const response = await fetch(
+				//`http://10.37.250.26:5000/panorama?lat=${lat}&lon=${lon}&zoom=${zoom}`
+				`https://pano.worldexcursions.click/panorama/cdn?lat=${lat}&lon=${lon}&zoom=${zoom}`
+			);
+			if (response.ok) {
+				const data = await response.body;
+				texture.load(data);
+				// const base64Image = data.base64Image;
+				// image.src = base64Image;
+				// Do something with the base64Image, e.g., display it on the page
+				// console.log(base64Image);
+
+				console.log(_location_list[_pos]);
+				resolve({
+					panorama_new: this.setPanorama(),
+					locationName: _location_list[_pos],
+					description: _desc_list[_pos],
+				});
+			} else {
+				// Handle HTTP errors
+				console.error(
+					"Error fetching panorama image:",
+					response.status,
+					response.statusText
+				);
+				reject(response);
+			}
+		});
+	}
 }
 
 export { PanoramaLoader };
